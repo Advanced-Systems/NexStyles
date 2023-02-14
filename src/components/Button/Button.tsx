ef6d8356e5c9a64f8d30e7b39fd0e5e0e41cde91
StefanGreve <greve.stefan@outlook.jp>
2023-02-14 17:39:35 +0100

    Set default props and add some basic style rules to components

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,25 @@
-import React from "react";
+import * as React from "react";
+import { v4 as uuidv4 } from "uuid";
 import { State } from "../../enumerations";
 import { IButton } from "../../interfaces";
 
 import "./Button.scss";
 
 class Button extends React.Component<IButton> {
+  public static defaultProps = {
+    id: `adv-btn-${uuidv4()}`,
+    className: '',
+    state: State.Success,
+  };
+
   render(): React.ReactNode {
     return (
       <button
-        className="adv-sys-button"
+        id={this.props.id}
+        className={["adv-sys-button", this.props.className].filter(Boolean).join(' ')}
+        style={this.props.style}
         onClick={this.props.onClick}
-        data-state={this.props.state ?? State.Success}
+        data-state={this.props.state}
       >
         {this.props.label}
       </button>
