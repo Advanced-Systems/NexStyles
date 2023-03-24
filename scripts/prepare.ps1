bf15b714e34c23a3db8d0edced79304f8b147526
StefanGreve <greve.stefan@outlook.jp>
2023-03-24 22:19:13 +0100

    dev: create commit message template and update settings

--- scripts/prepare.ps1
+++ scripts/prepare.ps1
@@ -2,8 +2,9 @@ using namespace System.IO
 
 Push-Location -Path $([Path]::GetDirectoryName($PSScriptRoot))
 
-Write-Host "(1/2) Configure commit message template locally" -ForegroundColor Yellow
+Write-Host "(1/2) Update local Git settings" -ForegroundColor Yellow
 git config --local commit.template ".gitmessage"
+git config --local core.whitespace cr-at-eol
 
 Write-Host "(2/2) Install Git hooks" -ForegroundColor Yellow
 husky install | Out-Null
