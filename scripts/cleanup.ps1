dc2978233a3f78803c2d3cbfcbea723210726225
StefanGreve <greve.stefan@outlook.jp>
2023-03-31 21:13:01 +0200

    dev: edit scripts in package.json
    * add local yarn cache clean step to cleanup.ps1
    * define yarn install script (inline)

--- scripts/cleanup.ps1
+++ scripts/cleanup.ps1
@@ -6,8 +6,11 @@ $LocalConfigFiles = @(
   ".vscode/"
 )
 
-Write-Host "(1/1) Cleanup build artifacts" -ForegroundColor Yellow
+Write-Host "(1/2) Cleanup build artifacts" -ForegroundColor Yellow
 $BuildArtifacts = Get-Content -Path ".gitignore" | Where-Object { $_ -notin $LocalConfigFiles }
 $BuildArtifacts | Remove-Item -Recurse -Force
+
+Write-Host "(2/2) Clean local cache" -ForegroundColor Yellow
+yarn cache clean
 
 Pop-Location
